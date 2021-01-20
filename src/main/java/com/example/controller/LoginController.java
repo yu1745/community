2ef0006c1997d7a41df9d567c0fc7f1cb5866c61
yu1745 <1658783310@qq.com>
2021-01-21 00:45:39 +0800

    add index and bootstrap

---
+++ src/main/java/com/example/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class LoginController {
+    @GetMapping({"index","index.html"})
+    String index(Model model){
+        return "index";
+    }
+}
