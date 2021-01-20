b0d4dcdb657936d78fb7d13108e60f46a731c3b5
yu1745 <1658783310@qq.com>
2021-01-21 07:06:40 +0800

    OAUTH

---
+++ src/main/java/com/example/dto/GithubUser.java
@@ -0,0 +1,12 @@
+package com.example.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GithubUserDTO {
+    private Long id;
+    private String name;
+    private String bio;
+}
